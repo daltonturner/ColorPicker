3476cfdaaa707c2d9696cd36e6f6a08deefdc488
Dalton Turner <turnerdalton055@gmail.com>
2023-04-05 21:20:04 -0700

    Changing to color picker site

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,39 @@
-import Link from 'next/link'
+import { useState } from 'react';
+import { ChromePicker } from 'react-color';
 
-export default function IndexPage() {
+export default function Home() {
+  const [color, setColor] = useState('#ffffff');
+  const [palette, setPalette] = useState([]);
+
+  const handleColorChange = (color) => {
+    setColor(color.hex);
+  };
+
+  const generateColorPalette = () => {
+    const colorCount = 5;
+    const newPalette = Array.from({ length: colorCount }, () => {
+      const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+      return `#${randomColor.padStart(6, '0')}`;
+    });
+    setPalette(newPalette);
+  };
+
   return (
     <div>
-      Hello World. <Link href="/about">About</Link>
+      <h1>Color Picker Example Site</h1>
+      <div style={{ marginTop: '1rem', textAlign: 'center' }}>
+        <button onClick={generateColorPalette}>Generate Color Palette</button>
+      </div>
+      <div style={{ display: 'flex', justifyContent: 'space-between', marginTop: '1rem' }}>
+        {palette.map((color, index) => (
+          <div key={index} style={{ textAlign: 'center' }}>
+            <ChromePicker color={color} disableAlpha />
+            <p>{color}</p>
+          </div>
+        ))}
+      </div>
     </div>
-  )
+  );
 }
+
+
